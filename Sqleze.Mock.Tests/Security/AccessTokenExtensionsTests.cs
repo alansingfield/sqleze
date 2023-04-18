b8bfe85c96e90a78ad79db73fc1fad66176f5eff
Alan Singfield <mail@didsburysoftware.co.uk>
2023-04-18 17:33:23 +0100

    Added tests to make sure AccessToken set before Open() call

---
+++ Sqleze.Mock.Tests/Security/AccessTokenExtensionsTests.cs
@@ -0,0 +1,70 @@
+﻿using DryIoc;
+using Sqleze.Registration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sqleze.Mock.Tests.Security;
+
+[TestClass]
+public class AccessTokenExtensionsTests
+{
+    [TestMethod]
+    public void AccessTokenAtRoot()
+    {
+        using var container = new Container().WithNSubstituteFallback();
+
+        container.RegisterSqlezeCore();
+        container.RegisterSqlezeConnectionStringProviders();
+
+        // Make a singleton MS.SqlConnection
+        var sqlConnection = Substitute.For<MS.SqlConnection>();
+        container.RegisterInstance<MS.SqlConnection>(sqlConnection);
+
+        _ = sqlConnection.DidNotReceive().AccessToken;
+
+        var sqlezeBuilder = container.Resolve<ISqlezeBuilder>();
+
+        sqlezeBuilder
+            .WithConnectionString("FAKE")
+            .WithAccessToken("XXX111")
+            .Connect().Sql("").ExecuteNonQuery();
+
+        Received.InOrder(() =>
+        {
+            _ = sqlConnection.Received(1).AccessToken = "XXX111";
+            sqlConnection.Open();
+        });
+    }
+
+    [TestMethod]
+    public void AccessTokenAfterBuilder()
+    {
+        using var container = new Container().WithNSubstituteFallback();
+
+        container.RegisterSqlezeCore();
+        container.RegisterSqlezeConnectionStringProviders();
+
+        // Make a singleton MS.SqlConnection
+        var sqlConnection = Substitute.For<MS.SqlConnection>();
+        container.RegisterInstance<MS.SqlConnection>(sqlConnection);
+
+        _ = sqlConnection.DidNotReceive().AccessToken;
+
+        var sqlezeBuilder = container.Resolve<ISqlezeBuilder>();
+
+        sqlezeBuilder
+            .WithConnectionString("FAKE")
+            .Build()
+            .WithAccessToken("XXX111")
+            .Connect().Sql("").ExecuteNonQuery();
+
+        Received.InOrder(() =>
+        {
+            _ = sqlConnection.Received(1).AccessToken = "XXX111";
+            sqlConnection.Open();
+        });
+    }
+}
