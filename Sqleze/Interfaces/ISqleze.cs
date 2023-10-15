b35115410035ba2efc577a80034911a23f8b0b31
Alan Singfield <mail@didsburysoftware.co.uk>
2023-10-15 10:07:17 +0100

    Add some comments

--- Sqleze/Interfaces/ISqleze.cs
+++ Sqleze/Interfaces/ISqleze.cs
@@ -3,6 +3,16 @@
 // Was originally ISqlezeConnectionFactory
 public interface ISqleze
 {
+    /// <summary>
+    /// Open the connection to the database. Note that the actual ADO calls
+    /// are deferred until a database connection is actually needed.
+    /// </summary>
+    /// <returns></returns>
     ISqlezeConnection Connect();
+    /// <summary>
+    /// Get back to the Builder which created this Factory to enable
+    /// reconfiguring with different options
+    /// </summary>
+    /// <returns></returns>
     ISqlezeBuilder Reconfigure();
 }
--- Sqleze/Interfaces/ISqlezeBuilder.cs
+++ Sqleze/Interfaces/ISqlezeBuilder.cs
@@ -5,5 +5,10 @@
 public interface ISqlezeBuilder
 {
     ISqlezeBuilder With<T>(Action<T, ISqlezeScope> configure);
+    /// <summary>
+    /// Builds the factory which can then be used to create multiple connections
+    /// to the database
+    /// </summary>
+    /// <returns></returns>
     ISqleze Build();
 }
